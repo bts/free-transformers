0c2b388a3a2787ed856a397ad05ca0a8412adec6
Brian Schroeder <bts@gmail.com>
2016-11-02 21:33:42 -0400

    Remove TODO about comonadic interpretation
    Dave Liang suggests that it's easier to just go with natural transformations
    unless you have a specific need for the comonadic approach:
    
    https://www.reddit.com/r/haskell/comments/5ajmq5/comonadic_interpreter_vs_natural_transformation/d9j736f/

--- src/FreeTransformers.hs
+++ src/FreeTransformers.hs
@@ -387,6 +387,3 @@ instance Category1 Interpreter where
 
 
 -- TODO: Using Codensity/CPS to increase efficiency
-
--- TODO: Experiment with functor `Pairing`s and cofree comonads:
--- http://dlaing.org/cofun/posts/coproducts_for_free_and_products_for_cofree.html)
